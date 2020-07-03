75d181229ed1b84ef96569a8d5f60ad3a8eddbed
Yweldler17 <ycweldler@gmail.com>
2020-07-02 20:04:58 -0400

    HW 53

---
+++ javascript/53/js/one.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var name = 'Yaakov Weldler';
+var email = 'Ycweldler@gmail.com';
+var agePrompt = 'Please enter your age';
+var userAge = prompt(agePrompt);
+
---
+++ javascript/53/js/two.js
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log(name);
+console.log(email);
+console.log('You are ' + userAge + ' years old!');
