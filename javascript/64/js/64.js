177e755b9fad6f8105842e061319f67965c175b8
Yweldler17 <ycweldler@gmail.com>
2020-09-06 10:50:27 -0400

    HW 64

--- javascript/64/js/64.js
+++ javascript/64/js/64.js
@@ -1,49 +1,53 @@
-'use strict';
+(function () {
 
-/*global $ */
+    'use strict';
 
-const msgDisplay = $('#msgDisplay');
-const allowSubmit = $('#allowSubmit');
-const submit = $('#submit');
-const first = $('#first');
-const last = $('#last');
-const address = $('#address');
-const email = $('#email');
-const phone = $('#phone');
-const accountInfo = $('#accountInfo');
-submit.prop('disabled', true);
+    /*global $ */
 
-allowSubmit.change(function () {
-    if (allowSubmit.is(':checked')) {
-        submit.prop('disabled', false);
-        clearDisplay();
-    } else {
+    const msgDisplay = $('#msgDisplay');
+    const allowSubmit = $('#allowSubmit');
+    const submit = $('#submit');
+    const first = $('#first');
+    const last = $('#last');
+    const address = $('#address');
+    const email = $('#email');
+    const phone = $('#phone');
+    const accountInfo = $('#accountInfo');
+    submit.prop('disabled', true);
+
+    allowSubmit.change(function () {
+        if (allowSubmit.is(':checked')) {
+            submit.prop('disabled', false);
+            clearDisplay();
+        } else {
+            submit.prop('disabled', true);
+        }
+    });
+
+    function clearDisplay() {
+        msgDisplay.text('');
+        accountInfo.text('');
+    }
+
+    function clearForm() {
+        first.val('');
+        last.val('');
+        address.val('');
+        email.val('');
+        phone.val('');
+        allowSubmit.prop('checked', false);
         submit.prop('disabled', true);
     }
-});
 
-function clearDisplay() {
-    msgDisplay.text('');
-    accountInfo.text('');
-}
+    submit.click(function (e) {
+        e.preventDefault();
+        let msg = first.val() + " ";
+        msg += last.val();
+        msgDisplay.text('Hello ' + msg + "! Your account information is below");
+        accountInfo.text("Address: " + address.val() + " - Email: " + email.val() + " - Phone:" + phone.val());
+        accountInfo.text();
+        clearForm();
 
-function clearForm() {
-    first.val('');
-    last.val('');
-    address.val('');
-    email.val('');
-    phone.val('');
-    allowSubmit.prop('checked', false);
-    submit.prop('disabled', true);
-}
+    });
 
-submit.click(function (e) {
-    e.preventDefault();
-    let msg = first.val() + " ";
-    msg += last.val();
-    msgDisplay.text('Hello ' + msg + "! Your account information is below");
-    accountInfo.text("Address: " + address.val() + " - Email: " + email.val() + " - Phone:" + phone.val());
-    accountInfo.text();
-    clearForm();
-
-});
+}());
