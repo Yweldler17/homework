9d56cfbd82d106bd047f4cc102d1ad564409d412
Yweldler17 <ycweldler@gmail.com>
2020-09-08 23:13:14 -0400

    HW 65

---
+++ javascript/65/65.js
@@ -0,0 +1,44 @@
+(function () {
+    'use strict';
+
+    /*global $ */
+
+    const fileForm = $('#fileForm');
+    const fileInput = $('#fileInput');
+    const displayFile = $('#fileInfo');
+    const submitButton = $('#submit');
+    const loadSpinner = $('.loader');
+    submitButton.prop('disabled', true);
+
+    fileInput.keyup(function () {
+        if (fileInput.val().length > 0) {
+            submitButton.prop('disabled', false);
+        } else {
+            submitButton.prop('disabled', true);
+        }
+    });
+
+    fileForm.submit(e => {
+        e.preventDefault();
+        loadSpinner.show();
+        setTimeout(() => {
+            let loadFile = fileInput.val();
+            fetch(loadFile)
+                .then(checkError)
+                .then(r => displayFile.text(r))
+                .catch(error => displayFile.text(error));
+            fileInput.val('');
+            loadSpinner.hide();
+            submitButton.prop('disabled', true);
+        }, 2000);
+    });
+
+    function checkError(response) {
+        if (response.status >= 400) {
+            throw Error(response.statusText);
+        } else {
+            return response.text();
+        }
+    }
+
+}());
