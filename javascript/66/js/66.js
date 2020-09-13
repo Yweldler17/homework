7c084f07c237eb1aa4bcf48f5fcdab973faa5acf
Yweldler17 <ycweldler@gmail.com>
2020-09-13 17:46:08 -0400

    HW 66

---
+++ javascript/66/js/66.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    /*global $ */
+
+    const recipe = $('#recipes');
+    const submitButton = $('#submit');
+    const ingredientsList = $('#ingredients');
+    const recipeName = $('#recipeName');
+    const recipeImg = $('#recipeImg');
+
+    submitButton.click(e => {
+        e.preventDefault();
+        let recipeChoice = recipe.val();
+        ingredientsList.empty();
+        fetch('66.json')
+            .then(r => r.json())
+            .then((r) => {
+                r.forEach(element => {
+                    if (element.name === recipeChoice) {
+                        displayRecipe(element);
+                    }
+                });
+            })
+            .catch(error => ingredientsList.text(error));
+    });
+
+    function displayRecipe(recipe) {
+        let name = recipe.name;
+        let img = recipe.img;
+        let ingredientsArray = recipe.ingredients;
+        let ingredientsInfo;
+        recipeName.text(name);
+        recipeImg.attr("src", img);
+        for (let i = 0; i < ingredientsArray.length; i++) {
+            addIngredient(ingredientsArray[i]);
+        }
+        ingredientsList.text(ingredientsInfo);
+    }
+
+    function addIngredient(ingredient) {
+        ingredientsList.append(`<li>${ingredient}</li>`);
+    }
+
+
+}());
