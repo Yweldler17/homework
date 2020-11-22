697f41debbdf37628e58baa2fb724912bab986c5
Yweldler17 <ycweldler@gmail.com>
2020-11-22 12:45:38 -0500

    Homework: 79 CSS Update

--- javascript/79/webpack.config.js
+++ javascript/79/webpack.config.js
@@ -5,5 +5,28 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            /*{
+              test: /\.(png|svg|jpg|jpeg|gif)$/i,
+              type: 'asset/resource',
+            }*/
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                        }
+                    }
+                ]
+            },
+        ]
     }
 };
